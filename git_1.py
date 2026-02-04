946f179089d8676dbcb9e6613b6956845570fc75
JonK05 <jonathanhc44@gmail.com>
2026-02-04 13:18:04 +0800

    Add files via upload
    test to upload git_1

---
+++ git_1.py
@@ -0,0 +1,14 @@
+'''
+first part for git
+created by : JonK
+date created: 04/02/2026
+'''
+
+def main():
+    welcome_str="Welcome to the program!"
+    print(welcome_str, end='\n')
+
+
+
+if __name__=="__main__":
+    main()
